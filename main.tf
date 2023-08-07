e1229be1788c6cae457722c126c26352a76fd6ed
anubhavvermauk1987 <86278138+anubhavvermauk1987@users.noreply.github.com>
2023-08-07 18:18:18 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -6,6 +6,6 @@ resource "aws_instance" "foo" {
   ami           = "ami-04336b6cac4075c5a"
   instance_type = "t3a.large"
   tags = {
-      Name = "TF-Instance-1"
+      Name = "TF-Instance-123"
   }
 }
