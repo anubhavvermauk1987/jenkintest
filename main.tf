a02edff338f37dc7f6c3e6e7f25c235435793eae
anubhavvermauk1987 <86278138+anubhavvermauk1987@users.noreply.github.com>
2023-08-10 14:22:52 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -6,6 +6,6 @@ resource "aws_instance" "foo" {
   ami           = "ami-0f5ee92e2d63afc18"
   instance_type = "t3a.large"
   tags = {
-      Name = "TF-Instance-testing"
+      Name = "TF-Instance-"
   }
 }
