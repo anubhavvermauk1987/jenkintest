7f7e362b693b851e348cbc7d93a793d37780d742
anubhavvermauk1987 <86278138+anubhavvermauk1987@users.noreply.github.com>
2023-08-10 14:32:37 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,7 +2,7 @@ provider "aws" {
     region = "ap-south-1"  
 }
 
-resource "aws_instance" "foo" {
+resource "aws_instance" "foo1" {
   ami           = "ami-04336b6cac4075c5a"
   instance_type = "t3.large"
   tags = {
