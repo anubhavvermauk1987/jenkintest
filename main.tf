f59cef0c595cf6d00c6e467ecb8694edecff088a
anubhavvermauk1987 <86278138+anubhavvermauk1987@users.noreply.github.com>
2023-08-10 14:26:14 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -6,6 +6,6 @@ resource "aws_instance" "foo" {
   ami           = "ami-0f5ee92e2d63afc18"
   instance_type = "t2.micro"
   tags = {
-      Name = "TF-Instance-"
+      Name = "TF-Instance-111"
   }
 }
