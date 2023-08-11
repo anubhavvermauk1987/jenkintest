36498af9990904dce0e1d0faa9db8424f1f08342
anubhavvermauk1987 <86278138+anubhavvermauk1987@users.noreply.github.com>
2023-08-11 15:16:42 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -4,7 +4,8 @@ provider "aws" {
 
 resource "aws_instance" "foo1" {
   ami           = "ami-04336b6cac4075c5a"
-  instance_type = "t3.large"
+  instance_type = "t2.micro"
+#instance_type = "t3.large"
   tags = {
       Name = "F-Instance-10-Aug-2024"
   }
